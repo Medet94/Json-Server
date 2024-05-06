a30d95e45c1760160711b01042495fb125e011a1
Medet <medet.berikbaev.94@gmail.com>
2024-05-06 22:27:54 +0500

    Co-authored-by: FarhodKurbonov <FarhodKurbonov@users.noreply.github.com>

--- src/components/ChatList.jsx
+++ src/components/ChatList.jsx
@@ -2,7 +2,6 @@ import React, { useContext } from 'react';
 import Chat from '../components/Chat';
 import PostContext from '../context/index';
 import { IoIosAdd } from 'react-icons/io';
-import { MdDoneOutline } from 'react-icons/md';
 
 function ChatList() {
   const {
--- src/url.js
+++ src/url.js
@@ -6,3 +6,5 @@ export const createNewPost = 'http://localhost:3001/posts';
 export const deletePostById = 'http://localhost:3001/posts/';
 export const getCommentById = 'http://localhost:3001/comments?id=';
 export const updatePostById = 'http://localhost:3001/posts/';
+
+export const deleteAllCommentsByPostId = 'http://localhost:3001/comments/';
